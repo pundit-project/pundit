3695fe40220e4b7aeb3ce52d5e7ac854c0343ffd
Danny Lee <dannylhy@gmail.com>
2015-09-14 18:39:44 -0400

    Removed wrongly merged code

--- pythia_agent_loc/pythia_daemon.pl
+++ pythia_agent_loc/pythia_daemon.pl
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!perl -w
 #
 # Copyright 2012 Georgia Institute of Technology
@@ -38,42 +37,3 @@ while(1)
 	#`perl tree.pl`;
 	`perl scandir.pl >> run.log 2>&1`;
 }
-
-=======
-#!perl -w
-#
-# Copyright 2012 Georgia Institute of Technology
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-use strict;
-
-use POSIX qw(setsid);
-
-umask 0;
-open STDIN, '/dev/null' or die "Can't read /dev/null: $!";
-open(STDOUT, ">>run.log") or die;
-open STDERR, '>>run.log' or die "Can't write to /dev/null: $!";
-defined(my $pid = fork) or die "Can't fork: $!";
-exit if $pid;
-setsid or die "Can't start a new session: $!";
-
-print "Starting server..\n";
-while(1)
-{
-	`perl tree.pl`;
-	`perl scandir.pl >> run.log 2>&1`;
-}
-
->>>>>>> branch 'master' of https://github.com/pundit-project/pundit.git
