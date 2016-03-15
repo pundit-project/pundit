3a290338d20eede070fbb01b564239ce3e34d038
Danny Lee <dannylhy@gmail.com>
2016-03-15 11:44:39 -0400

    Added test suite for Reporter::MySQL

---
+++ pundit_central/tests/ReporterMySQLTest.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl
+#
+# Copyright 2016 Georgia Institute of Technology
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+use strict;
+use Config::General;
+
+use FindBin qw( $RealBin );
+
+use lib "$RealBin/../lib";
+
+use Localization::Reporter::MySQL;
+use Utils::TrHop;
+
+# debug. Remove this for production
+use Data::Dumper;
+
+=pod
+
+=head1 DESCRIPTION
+
+This is a test library for verifying Reporter::Range behaviour
+
+=cut
+
+my $configFile = $RealBin . "/../etc/pundit_central.conf";
+my %cfgHash = Config::General::ParseConfig($configFile);
+my $fedName = 'federation1';
+
+my $reporter = new Localization::Reporter::MySQL(\%cfgHash, $fedName);
+my $trHop1 = new Utils::TrHop();
+$trHop1->addHopEntry("a","1.1.1.1");
+my $trHop2 = new Utils::TrHop();
+$trHop2->addHopEntry("b","1.1.1.2");
+$trHop2->addHopEntry("c","1.1.1.3");
+
+my $ev1 =
+    {
+        'link' => $trHop1,
+        'range' => [2.5, 2.75],
+    };
+my $ev2 =    
+    {
+        'link' => $trHop2,
+        'range' => [2.5, 3.75],
+    };
+
+$reporter->writeData(time - 100, "boolean", 1, $ev1);
+$reporter->writeData(time - 50, "range_sum",2, $ev2);
