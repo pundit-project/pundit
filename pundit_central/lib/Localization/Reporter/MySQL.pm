9a57c34d11e27659898b5fc78be508b772200c81
Danny Lee <dannylhy@gmail.com>
2016-03-15 11:43:15 -0400

    Consolidated Reporter submodules to be backend specific (MySQL)

---
+++ pundit_central/lib/Localization/Reporter/MySQL.pm
@@ -0,0 +1,100 @@
+#!/usr/bin/perl
+#
+# Copyright 2016 Georgia Institute of Technology
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package Localization::Reporter::MySQL;
+
+use strict;
+use DBI;
+
+# debug. Remove this for production
+#use Data::Dumper;
+
+# Creates a new object
+sub new
+{
+	my ($class, $cfgHash, $fedName) = @_;
+        
+	# init the DBI
+	my $host = $cfgHash->{'pundit_central'}{$fedName}{'reporting'}{'mysql'}{"host"};
+    my $port = $cfgHash->{'pundit_central'}{$fedName}{'reporting'}{'mysql'}{"port"};
+    my $database = $cfgHash->{'pundit_central'}{$fedName}{'reporting'}{'mysql'}{"database"};
+    my $user = $cfgHash->{'pundit_central'}{$fedName}{'reporting'}{'mysql'}{"user"};
+    my $pw = $cfgHash->{'pundit_central'}{$fedName}{'reporting'}{'mysql'}{"password"};
+    
+	my $dbh = DBI->connect("DBI:mysql:$database:$host:$port", $user, $pw) or return undef;
+	
+	# Create the table if it doesn't exist
+	$dbh->do("CREATE TABLE IF NOT EXISTS localization_events (
+    	ts TIMESTAMP,
+    	link_ip INT UNSIGNED,
+    	link_name VARCHAR(256),
+    	det_code TINYINT UNSIGNED,
+    	val1 INT UNSIGNED NULL,
+    	val2 INT UNSIGNED NULL
+    	);");
+	
+	my $sql = "INSERT INTO localization_events (ts, link_ip, link_name, det_code, val1, val2) VALUES (FROM_UNIXTIME(?), INET_ATON(?), ?, ?, ?, ?)";
+	
+	return undef if (!defined($sql));
+	my $sth = $dbh->prepare($sql) or return undef;
+	
+	my $self = {
+        _dbh => $dbh,
+        _sth => $sth,
+    };
+    
+    bless $self, $class;
+    return $self;
+}
+
+
+# Cleanup
+sub DESTROY
+{
+    my $self = shift;
+    
+    my $sth = $self->{'_sth'};
+    my $dbh = $self->{'_dbh'};
+    
+    $sth->finish if $sth;
+    $dbh->disconnect if $dbh;
+}
+
+
+# Writes the result array to the database
+sub writeData
+{
+	my ($self, $start_time, $tomo, $detectionCode, $event) = @_;
+	
+	my $sth = $self->{'_sth'};
+	my $dbh = $self->{'_dbh'};
+	
+	my $trHop = $event->{'link'};
+	foreach my $hopInfo (@{$trHop->getRawList()})
+	{
+	    if ($tomo eq "range_sum")
+        {
+            $sth->execute($start_time, $hopInfo->{'hopIp'}, $hopInfo->{'hopName'}, $detectionCode, int($event->{'range'}[0] * 10), int($event->{'range'}[1] * 10)) or print "$dbh->errstr\n";
+        }
+        elsif ($tomo eq "boolean")
+        {
+            $sth->execute($start_time, $hopInfo->{'hopIp'}, $hopInfo->{'hopName'}, $detectionCode, undef, undef) or print "$dbh->errstr\n";
+        }
+	}
+}
+
+1;
