a4c45eaf1be83936832995671838cbc1e6d32084
Danny Lee <dannylhy@gmail.com>
2016-04-27 18:20:46 -0400

    Added support files

---
+++ pundit_central/system/mysql/create_tables.sql
@@ -0,0 +1,60 @@
+#
+# Copyright 2016 Georgia Institute of Technology
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# create_tables.sql
+#
+# Run this to create the tables needed for PuNDIT's normal operation
+#
+
+create database pythia;
+use pythia;
+
+# Statuses reported from agents
+
+CREATE TABLE `status` (
+  `startTime` int(11) DEFAULT NULL,
+  `endTime` int(11) DEFAULT NULL,
+  `srchost` varchar(256) DEFAULT NULL,
+  `dsthost` varchar(256) DEFAULT NULL,
+  `baselineDelay` float DEFAULT NULL,
+  `detectionCode` int(11) DEFAULT NULL,
+  `queueingDelay` float DEFAULT NULL,
+  `lossRatio` float DEFAULT NULL,
+  `reorderMetric` float DEFAULT NULL
+)
+
+# localization results
+
+CREATE TABLE `localization_events` (
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `link_ip` int(10) unsigned DEFAULT NULL,
+  `link_name` varchar(256) DEFAULT NULL,
+  `det_code` tinyint(3) unsigned DEFAULT NULL,
+  `val1` int(10) unsigned DEFAULT NULL,
+  `val2` int(10) unsigned DEFAULT NULL
+)
+
+# Traceroute table for holding traces collected by each agent
+
+CREATE TABLE `traceroutes` (
+  `ts` int(32) NOT NULL,
+  `src` varchar(256) DEFAULT NULL,
+  `dst` varchar(256) DEFAULT NULL,
+  `hop_no` int(32) NOT NULL,
+  `hop_ip` varchar(256) DEFAULT NULL,
+  `hop_name` varchar(256) DEFAULT NULL
+);
+
