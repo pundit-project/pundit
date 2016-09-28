92532eb2b1a4e3ec4646bee4469a0d888f15c21b
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2016-09-28 15:42:02 -0400

    Precalculate time series in a table, instead of using a view

---
+++ processTimeSeries.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import mysql.connector
+import time
+
+cnx = mysql.connector.connect(user='root', password='pythiaRush!', database='pythia_new')
+
+cursor = cnx.cursor(buffered=True)
+
+createTimeSeries = """CREATE TABLE timeSeries SELECT FROM_UNIXTIME(FLOOR((UNIX_TIMESTAMP(startTime) / (5 * 60))) * (5 * 60)) AS timeBlock, srcId AS srcId, dstId AS dstId, MAX(queueingDelay) AS delay, MAX(lossRatio) AS loss, MAX(((detectionCode & 2) <> 0)) AS hasDelay, MAX(((detectionCode & 4) <> 0)) AS hasLoss FROM status GROUP BY timeBlock, srcId, dstId ORDER BY srcId, dstId, startTime"""
+
+start = time.time();
+print "Recalculate time series"
+cursor.execute("DROP TABLE IF EXISTS timeSeries");
+cursor.execute(createTimeSeries);
+end = time.time();
+print "Done in %s s" %(str(end-start));
