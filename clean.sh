e39693e5a8bfe9a2b195839b7b064b80e8a8b565
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2017-05-24 10:09:46 -0400

    Adding clean script

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Make sure we are in the correct directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+# Clean build directories
+rm -rf pundit_ui/build
+
+# Clean all intermedite files from rpmbuild
+cd rpmbuild
+git clean -xdf
