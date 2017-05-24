f17b7d80e0240e2b7c87c7531500fb71d2fc9d70
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2017-05-24 09:56:52 -0400

    Added build script that creates the tarball for pundit-ui

---
+++ pundit_ui/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Make sure we are in the correct directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+# Clean build directory if present
+rm -rf build
+mkdir build
+mkdir build/pundit-ui
+
+# Copy diirt web app and configutaion
+cp -r diirt build/pundit-ui
+
+# Copy web ui
+cp -r web-ui/public_html/ build/pundit-ui/
+
+# Create tarball
+cd build
+tar -zcf pundit-ui.tar.gz pundit-ui
+
+# Copy to rpmbuild/SOURCES
+cd ../..
+cp pundit_ui/build/pundit-ui.tar.gz rpmbuild/SOURCES
