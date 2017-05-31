fcf8475aac98f6c4065229865cad42432424fd64
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2017-05-31 11:04:02 -0400

    Adding RabbitMQ rpms to the pundit yum repository

--- build.sh
+++ build.sh
@@ -18,4 +18,9 @@ echo Building pundit-ui...
 echo Building rpms...
 ./rpmbuild/build.sh
 
- 
+echo Collecting rpm dependencies...
+cd rpmbuild/RPMS/
+# Take minimal Erlang as created for RabbitMQ
+wget https://github.com/rabbitmq/erlang-rpm/releases/download/v19.3.4/erlang-19.3.4-1.el6.x86_64.rpm
+# Take RabbitMQ
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/rabbitmq_v3_6_10/rabbitmq-server-3.6.10-1.el6.noarch.rpm
