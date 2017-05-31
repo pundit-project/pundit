cef52ed58dba2004a9444d73a9ccb7fef3d7e231
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2017-05-31 10:37:47 -0400

    Adding perl RabbitMQ to the build

--- rpmbuild/build.sh
+++ rpmbuild/build.sh
@@ -4,6 +4,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
+rpmbuild --define "_topdir `pwd`" -ba SPECS/perl-Net-RabbitMQ.spec
 rpmbuild --define "_topdir `pwd`" -ba SPECS/pundit-central.spec
 spectool -g -C SOURCES SPECS/glassfish4.spec
 rpmbuild --define "_topdir `pwd`" -ba SPECS/glassfish4.spec
